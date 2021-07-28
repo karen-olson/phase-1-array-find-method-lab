07fb03071bf23c870efc552f2774384a74c16186
Karen Olson <karmarie@gmail.com>
2021-07-28 16:39:11 -0500

    commit tree f821c4fd9379643c51eda16867d7ddb01cb6d52b

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
+const record = [
+  { year: "2018", result: "N/A" },
+  { year: "2017", result: "N/A" },
+  { year: "2016", result: "N/A" },
+  { year: "2015", result: "N/A" },
+  { year: "2014", result: "N/A" },
+  { year: "2013", result: "N/A" },
+  { year: "2012", result: "N/A" },
+  { year: "2011", result: "N/A" },
+  { year: "2010", result: "N/A" },
+  { year: "2009", result: "N/A" },
+  { year: "2008", result: "N/A" },
+  { year: "2007", result: "N/A" },
+  { year: "2006", result: "N/A" },
+  { year: "2005", result: "N/A" },
+  { year: "2004", result: "N/A" },
+  { year: "2003", result: "N/A" },
+  { year: "2002", result: "N/A" },
+  //{ year: "2001", result: "W" }
+];
 
+function superbowlWin(array) {
+  function isWin(item) {
+    if (item.result === "W") {
+      return true;
+    }
+  }
+
+  const winningItem = array.find(isWin);
+
+  if (typeof winningItem === "object") {
+    return winningItem.year;
+  } else {
+    return winningItem;
+  }
+}
+
+const winningYear = superbowlWin(record);
+console.log(winningYear);
